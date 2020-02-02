70ba54d6a5fca1945fedd4859d4a1a40fdf99c06
Ivan1 <kiievets1ivan@hneu.net>
2020-02-02 19:08:18 +0200

    codeFirst(EFCore)

---
+++ WebApplication1/WebApplication1/Models/UserRepository.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class UserRepository : DbContext
+    {
+        public UserRepository(DbContextOptions<UserRepository> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> UserSet { get; set; }
+    }
+}
